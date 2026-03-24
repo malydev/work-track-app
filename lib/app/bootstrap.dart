c733d4f6dd95edbebfb7fdd11bc3de113e045693
malydev <miguellipa1@gmail.com>
2026-03-23 21:24:05 -0400

    refactor: create shared constants and bootstrap adjustments

--- lib/app/bootstrap.dart
+++ lib/app/bootstrap.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:work_track/app/app.dart';
@@ -6,6 +9,18 @@ import 'package:work_track/core/storage/hive_initializer.dart';
 Future<void> bootstrap() async {
   WidgetsFlutterBinding.ensureInitialized();
   await HiveInitializer.initialize();
+
+  FlutterError.onError = (details) {
+    FlutterError.presentError(details);
+    debugPrint('FlutterError: ${details.exceptionAsString()}');
+    debugPrintStack(stackTrace: details.stack);
+  };
+
+  PlatformDispatcher.instance.onError = (error, stack) {
+    debugPrint('PlatformDispatcherError: $error');
+    debugPrintStack(stackTrace: stack);
+    return true;
+  };
 
   runApp(const ProviderScope(child: WorkTrackApp()));
 }
